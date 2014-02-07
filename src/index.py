d02de495d2af1854e526d824c74eaa5ea30dde1d
lekez2005 <lekez2005@yahoo.ca>
2014-02-07 18:46:11 -0500

    Added index page

--- src/index.py
+++ src/index.py
@@ -9,22 +9,13 @@ import json
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/assets/templates"),
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/static/templates"),
     extensions=['jinja2.ext.autoescape'])
-class Question(webapp2.RequestHandler):
-    def get(self):
-        question = db.GqlQuery("SELECT * FROM Objective").fetch(1)[0]
-        self.response.headers['Content-Type'] = 'application/json'
-        obj = {'question': question.question,
-               'answer': question.question}
-        self.response.out.write(json.dumps(obj))
+
         
         
 class Index(webapp2.RequestHandler):
     def get(self):
-        #self.initialize_database()
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        #questions = db.GqlQuery("SELECT * from  Objective")
-        #self.response.write(template.render({'questions':questions}))
-        self.response.write(template.render(title = 'Past Questions'))
+        self.response.write(template.render(title = 'College Essays'))
     
